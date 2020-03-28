064c161a689fe0f7ffd6023f5841cb93f60f8b56
Waseem Awashra <wawashra@gmail.com>
2020-03-28 19:49:04 +0300

    ser done

--- src/main/webapp/app/layouts/search/search.component.ts
+++ src/main/webapp/app/layouts/search/search.component.ts
@@ -1,66 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { CourseService } from 'app/entities/course/course.service';
 import { HttpResponse } from '@angular/common/http';
 import { ICourse } from 'app/shared/model/course.model';
-
-export interface State {
-  flag: string;
-  name: string;
-  population: string;
-}
-
+import { Observable } from 'rxjs';
 @Component({
   selector: 'jhi-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  totalItems = 0;
-  stateCtrl = new FormControl();
   filteredStates?: Observable<ICourse[]>;
-  courses?: ICourse[];
-  itemsPerPage = 10;
-  page!: number;
-  predicate!: string;
-  ascending!: boolean;
-  ngbPaginationPage = 1;
+  stateCtrl = new FormControl();
   states: ICourse[] = [];
+  course = '';
 
   constructor(protected courseService: CourseService) {}
 
-  private was(value: string): ICourse[] {
-    this.courseService
-      .query({
-        'name.contains': value,
-        'symbol.contains': value
-      })
-      .subscribe((res: HttpResponse<ICourse[]>) => {
-        this.states = res.body || [];
-        return this.states;
-      });
-    return [];
-  }
-
   ngOnInit(): void {
-    this.courseService.query().subscribe((res: HttpResponse<ICourse[]>) => {
-      this.states = res.body || [];
-
-      this.filteredStates = this.stateCtrl.valueChanges.pipe(map(state => (state ? this._filterStates(state) : this.states.slice())));
+    this.stateCtrl.valueChanges.subscribe(newValue => {
+      this.filterICourse(newValue);
     });
   }
 
-  private _filterStates(value: string): ICourse[] {
+  private filterICourse(value: string): void {
+    const filterValue = value.toLowerCase();
     this.courseService
       .query({
-        'name.contains': value,
-        'symbol.contains': value
+        'name.contains': filterValue
       })
       .subscribe((res: HttpResponse<ICourse[]>) => {
-        return res.body;
+        this.states = res.body || [];
+        this.filteredStates = new Observable<[]>();
       });
-    return [];
   }
 }
