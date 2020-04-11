fe4233dc7b9148c85f1552228e144cd939db85d0
Waseem Awashra <wawashra@gmail.com>
2020-04-11 20:07:47 +0300

    fix file to blob

--- src/main/webapp/app/entities/uploader/uploader.component.ts
+++ src/main/webapp/app/entities/uploader/uploader.component.ts
@@ -16,7 +16,7 @@ export class UploaderComponent {
 
   onDrop(files: FileList): void {
     for (let i = 0; i < files.length; i++) {
-      this.files.push(files.item(i));
+      this.files.push(files[i]);
     }
   }
 }
